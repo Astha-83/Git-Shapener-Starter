884ff51650992274b018c2a84a24492698afb58a
Astha-83 <astha6743@gmail.com>
2023-04-14 00:41:30 +0530

    update

--- dom.js
+++ dom.js
@@ -5,6 +5,25 @@ header.style.borderBottom = 'solid 3px #000';
 var title = document.getElementById('title');
 // title.style.color = 'green';
 // console.log (title)
-for (var i =0;i  < title.length ; i++){
+for (var i =0;i  < title ; i++){
     title[i].style.color   = 'green';
 }
+var items = document.getElementsByClassName('list-group-items');
+console.log(items);
+console.log(items[4]);
+for (var i =0;i  < items.length ; i++){
+    items[i].style.color   = 'green';
+
+}
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[4]);
+li[4].textContent ="Hello "
+li[4].style.fontWeight ="bold "
+for (var i =0;i  < li.length ; i++){
+    li[i].style.backgroundColor   = '#f4f4f4';
+
+}
+
+
+
